5e00c21797543f9cb54f35523a69f046035045eb
lwj0712 <dldnjswot@naver.com>
2024-08-14 15:20:16 +0900

    🔨 add SearchHistory

--- chat/urls.py
+++ chat/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('chat/', views.ChatView.as_view(), name='chat'),  # ChatGPT 서비스 페이지
     path('login/', auth_views.LoginView.as_view(template_name='chat/login.html'), name='login'),  # 로그인 페이지
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),  # 로그아웃 후 홈으로 리디렉션
+    path('history/', views.SearchHistoryView.as_view(), name='search_history'),
+    # 로그인/로그아웃 URL 등 추가
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import FormView
 from .forms import ChatForm
+from .models import SearchHistory
 from openai import OpenAI
 from django.conf import settings
-from django.views.generic import TemplateView
-import fitz # PyMuPDF
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView
+import fitz  # PyMuPDF 라이브러리
 from youtube_transcript_api import YouTubeTranscriptApi
 
 # 클라이언트 인스턴스 생성
@@ -36,32 +37,27 @@ def translate_to_korean(text, model="gpt-3.5-turbo"):
         return f"An error occurred during translation: {e}"
 
 def extract_text_from_pdf(pdf_file):
-    # PDF파일에서 텍스트를 추출 함수
     text = ""
     with fitz.open(stream=pdf_file.read(), filetype="pdf") as doc:
         for page in doc:
             text += page.get_text()
-    return text   
+    return text
 
 def extract_text_from_youtube(url):
-    # Youtube URL에서 자막(스크립트를)을 추출하는 함수
-    # http://www.youtube.com/watch?v=
-    video_id = url.split('v=')[-1] # Youtube URL에서  video_id 추출
-    # get_transcript 해당하는 비디오 ID를 기반으로 동영상 자막을 가져오게 됨
+    video_id = url.split('v=')[-1]  # URL에서 비디오 ID 추출
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
-    # 자막 텍스트를 하나의 문자열로 결합하는 과정 -> 리스트컴프리핸션! 공백으로 연결해서 하나의 텍스트로 만들어줌
     transcript_text = ' '.join([item['text'] for item in transcript])
     return transcript_text
-    
-def generate_prompt(text_input = None, file_input = None, youtube_url = None):
+
+def generate_prompt(text_input=None, file_input=None, youtube_url=None):
     if text_input:
         return f"다음 내용을 10줄 이내로 요약해줘: {text_input}"
     elif file_input:
         file_content = extract_text_from_pdf(file_input)
-        return f"다음 PDF파일 내용을 10줄 이내로 요약해줘: {file_content}"
+        return f"다음 PDF 파일 내용을 10줄 이내로 요약해줘: {file_content}"
     elif youtube_url:
         youtube_content = extract_text_from_youtube(youtube_url)
-        return f"다음 Youtube 동영상 내용을 10줄 이내로 요약해줘: {youtube_content}"
+        return f"다음 YouTube 동영상 내용을 10줄 이내로 요약해줘: {youtube_content}"
     else:
         return "요약할 내용이 없습니다."
 
@@ -99,3 +95,10 @@ class ChatView(LoginRequiredMixin, FormView):
 class HomeView(TemplateView):
     template_name = 'chat/home.html'
 
+class SearchHistoryView(LoginRequiredMixin, ListView):
+    model = SearchHistory
+    template_name = 'chat/search_history.html'
+    context_object_name = 'search_histories'
+
+    def get_queryset(self):
+        return SearchHistory.objects.filter(user=self.request.user).order_by('-created_at')
